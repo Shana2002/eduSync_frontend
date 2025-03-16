65e44ae4ead1e4ece6d3b791ed37a9370d890173
Shana2002 <hansakaravi02@gmail.com>
2025-03-16 14:18:58 +0530

    assigment batch managemet

--- admin/js/navigation.js
+++ admin/js/navigation.js
@@ -25,3 +25,13 @@ document.getElementById('close-btn-panel').addEventListener("click",(e)=>{
     e.preventDefault();
     document.getElementById('batch-details-panel').classList.remove('toglle-batch');
 })
+
+document.getElementById('assigment-manage-btn').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('assigment-panel').classList.add('toggle-assigment');
+})
+
+document.getElementById('back-assigment-mange').addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById('assigment-panel').classList.remove('toggle-assigment');
+})
